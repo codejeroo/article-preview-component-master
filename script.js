a706a4a5ba56e35a0ea7274ec97b65d85a91620c
Codejeroo <gonzaga.angelor@gmail.com>
2025-03-10 12:59:29 +0800

    Added basic popup

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 const shareButton = document.querySelectorAll(".share-button");
 const cardFooter = document.querySelector(".card-footer")
 const sharePopup = document.querySelector(".share-popup")
-
-
+const shareIcon = document.querySelector(".share-icon2")
+const activeShareButton = document.querySelector(".share-btn")
 
 shareButton.forEach(button => {
-
     button.addEventListener("click", function() {
-        const isActive = sharePopup.classList.toggle("active")
-        cardFooter.classList.toggle("hidden", isActive)
-        cardFooter.computedStyleMap.display = "none";
+        const isActive = sharePopup.classList.toggle("active");
+        cardFooter.classList.toggle("hidden", isActive);
+
+        if (isActive) {
+            activeShareButton.style.backgroundColor = "#6E8098";
+            shareIcon.style.filter = "invert(100%) brightness(1000%)";
+        } else {
+            activeShareButton.style.backgroundColor = "";
+            shareIcon.style.filter = "";
+        }
     });
-    
 });
